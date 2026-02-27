40a368da4666532266ce1a67aa76a7c0637cc0cd
venkatkalyan23 <koppala.venkatkalyan23@gmail.com>
2026-02-27 23:06:39 +0530

    Add modified buildspec.yml

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//controller
 @RestController
 public class TestController {
     @GetMapping("/version")
