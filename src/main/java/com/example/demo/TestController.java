bcf6ee92b87a5fb699049529bdcc6a15ba4aa4fe
venkatkalyan23 <koppala.venkatkalyan23@gmail.com>
2026-02-27 23:52:47 +0530

    commit

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/version")
     public String getVersion(){
-        return "dev";
+        return "dev main";
     }
 
      @GetMapping("/")
