4f57c3e90789f3f593c850b4b92ad4c66a49168a
venkatkalyan23 <koppala.venkatkalyan23@gmail.com>
2026-02-27 22:43:00 +0530

    Add Spring boot application

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/version")
     public String getVersion(){
-        return "prod";
+        return "dev";
     }
 }
